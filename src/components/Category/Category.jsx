a638ca9f0c552155aaafd1c1e4ef92e2dfad030b
pranav gosai <115298514+pranavgosai@users.noreply.github.com>
2023-01-22 15:48:42 +0530

    Add files via upload

---
+++ src/components/Category/Category.jsx
@@ -0,0 +1,12 @@
+import "./Category.scss";
+const Category = () => {
+    return <div className="category-main-content">
+        <div className="layout">
+            <div className="category-title">
+                
+            </div>
+        </div>
+    </div>;
+};
+
+export default Category;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import {Provider} from 'react-redux'
-import store from "./store";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
 import App from "./App";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
